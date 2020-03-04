2df1061ff26c80284e18c9da809f4a0b4b3bd530
Rafael C Fernandes <rafaelcostaf.rf@gmail.com>
2020-03-04 13:24:13 -0300

    Atualização ex1

--- comp_vis/lab01/ex_1/ex1.py
+++ comp_vis/lab01/ex_1/ex1.py
@@ -3,18 +3,15 @@ import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages')
 import cv2
 
-img = cv2.imread('image0.jpeg')
+img = cv2.imread('image1.jpeg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 h, w, _ = img.shape
 
 src_ver = np.zeros((h, w, 3), np.uint8)
-src_ver[:] = (255,0,255)
+src_ver[:] = (120,0,20)
 
 gray_alt = cv2.cvtColor(cv2.absdiff(img,src_ver), cv2.COLOR_BGR2GRAY)
-
-print(src_ver.shape)
-print(img.shape)
 
 cv2.imshow('image',img)
 cv2.imshow('gray',gray)
