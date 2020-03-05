ceebbaec132169b3759810914a6eaa95c9448f88
Rafael C Fernandes <rafaelcostaf.rf@gmail.com>
2020-03-05 18:01:20 -0300

    Final da Aula

--- comp_vis/lab01/ex2/ex2.py
+++ comp_vis/lab01/ex2/ex2.py
@@ -5,25 +5,31 @@ sys.path.append('/usr/local/lib/python3.7/site-packages')
 import cv2
 
 cap = cv2.VideoCapture(0)
-fps = 15.0
+
+cap.set(3, 320)
+cap.set(4, 240)
+cap.set(15, -5)
+cap.set(10, 120)
+
+fps = 10.0
+
 print("O framerate da webcam: " + str(fps))
 
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('saidavideo.avi',fourcc,fps,(640,480))
+out = cv2.VideoWriter('saidavideo.avi',fourcc,fps,(320,240))
 
 inicio = time.perf_counter()
 while (cap.isOpened()):
 	ret, frame = cap.read()
-	if ret == True:		
+	if ret == True:
 		if time.perf_counter()-inicio > 1/fps:
 			inicio = time.perf_counter()
-			out.write(frame)		
-			cv2.imshow('frame',frame)	
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+			out.write(frame)
+			cv2.imshow('frame',frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
 	else:
-		break
-        
+		break 
 cap.release()
 out.release()
 cv2.destroyAllWindows()
@@ -35,15 +41,12 @@ while(cap.isOpened()):
 	ret, frame = cap.read()
 	if ret == True:		
 		cv2.imshow('frame',frame)
-		cv2.waitKey(1)
 	else:
 		break
 	if 1/fps-(time.perf_counter()-inicio) < 0:
-		dorme = 0
-		print("atraso")
+		cv2.waitKey(1)
 	else:
 		dorme = 1/fps-(time.perf_counter()-inicio)
-	time.sleep(dorme)
-	
+		cv2.waitKey(int(dorme*1000))
 cap.release()
 cv2.destroyAllWindows()
